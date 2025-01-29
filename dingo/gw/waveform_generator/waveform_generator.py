cd87090e9551fb28740dd690dd490967bf513ddc
sclyne623 <149837291+sclyne623@users.noreply.github.com>
2025-01-29 07:43:46 -0500

    Update waveform_generator.py
    Added support for SEOBNRv5ROM which does not require the New Waveform Generator

--- dingo/gw/waveform_generator/waveform_generator.py
+++ dingo/gw/waveform_generator/waveform_generator.py
@@ -77,7 +77,7 @@ class WaveformGenerator:
             self.approximant_str = approximant
             self.lal_params = None
             self.mode_list = None
-            if "SEOBNRv5" not in approximant:
+            if "SEOBNRv5" not in approximant or "SEOBNRv5_ROM" in approximant:
                 # This LAL function does not work with waveforms using the new interface. TODO: Improve the check.
                 self.approximant = LS.GetApproximantFromString(approximant)
                 if mode_list is not None:
