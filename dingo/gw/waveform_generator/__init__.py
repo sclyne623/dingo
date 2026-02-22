bad2731cdaba9ba1c26814af4b6a9b2fad17fe9e
Sam Clyne <samclyne@mac.mynetworksettings.com>
2026-02-22 13:22:43 -0500

    Export BBHxWaveformGenerator and UniformFrequencyDomain from waveform_generator module

--- dingo/gw/waveform_generator/__init__.py
+++ dingo/gw/waveform_generator/__init__.py
@@ -2,6 +2,8 @@ from .waveform_generator import (
     WaveformGenerator,
     LISAWaveformGenerator,
     NewInterfaceWaveformGenerator,
+    BBHxWaveformGenerator,
     generate_waveforms_parallel,
     sum_contributions_m,
 )
+from .domain import UniformFrequencyDomain
